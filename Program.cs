a69bdfa24991a68c7d0a8ce0a19d26ca9d7de019
bhiseamar811@gmail.com <bhiseamar811@gmail.com>
2023-03-19 12:15:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            patterns printTables = new patterns();
+            // printTables.MultiplicationTable();
+            printTables.Printfectoriyals();
+
+
+            int numbers, i, counter, num1, num2;
+
+            
+            Console.WriteLine("Finding the prime numbers ------------------------------");
+          
+            Console.WriteLine("Input The First Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input The Last Number : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+           
+
+            for (numbers = num1; numbers <= num2; numbers++)
+            {
+                counter = 0;
+
+                for (i = 2; i <= numbers / 2; i++)
+                {
+                    if (numbers % i == 0)
+                    {
+                        counter++;
+                        
+                    }
+                }
+
+                if (counter == 0 )
+                {
+                    Console.WriteLine("Prime number is  " + numbers); 
+                }
+            }
+
+
+
+
+        }
+    }
+}
---
+++ patterns.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    internal class patterns
+    {
+        int num1, num2;
+        
+
+        public void MultiplicationTable()
+        {
+            
+                Console.WriteLine("Please enter the value for columns");
+                int A = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the value for rows");
+                int B = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter row and columns");
+
+
+
+
+                for (int i = 1; i <= B; i = i + 1)
+                {
+
+                    for (int j = 2; j <= A; j = j + 1)
+                    {
+                        Console.Write(i * j + "  |");
+
+                    }
+                    Console.WriteLine();
+                }
+
+            
+
+        }
+        
+
+    }
+}
